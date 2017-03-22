ed8c9f5fdaf89e308d31cd68d4cd73e868d23c34
Jizi Zhang <zjzottawa@gmail.com>
2017-03-21 23:44:05 -0400

    Path class added using test driven development

---
+++ TreehouseDefense/Path.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+	public class Path
+	{
+		private MapLocation[] pathLocations;
+
+		public Path(MapLocation[] pathLocations)
+		{
+			this.pathLocations = pathLocations;
+		}
+
+		public bool IsOnPath(MapLocation mapLocation)
+		{
+			return Array.IndexOf(pathLocations, mapLocation) >= 0;
+		}
+	}
+}
---
+++ TreehouseDefenseTest/PathTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace TreehouseDefense.Test
+{
+	public class PathTests
+	{
+		private Map _map3x3;
+		private MapLocation[] _pathLocations3;
+		private Path _path3;
+
+		public PathTests()
+		{
+			_map3x3 = new Map(3, 3);
+
+			_pathLocations3 = new MapLocation[]
+			{
+				new MapLocation(0, 1, _map3x3),
+				new MapLocation(1, 1, _map3x3),
+				new MapLocation(2, 1, _map3x3)
+			};
+
+			_path3 = new Path(_pathLocations3);
+		}
+
+		[Fact]
+		public void MapLocationIsOnPath()
+		{
+			var target = _path3;
+
+			Assert.True(target.IsOnPath(new MapLocation(1, 1, _map3x3)));
+		}
+
+		[Fact]
+		public void MapLocationIsNotOnPath()
+		{
+			var target = _path3;
+
+			Assert.False(target.IsOnPath(new MapLocation(2, 2, _map3x3)));
+		}
+	}
+}
